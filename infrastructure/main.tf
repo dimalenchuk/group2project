9878fb943de214046a8ffe735cd2e968d7a1fafd
dimalenchuk <dimalenchuk@gmail.com>
2020-12-03 23:16:45 +0200

    change zone

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,7 +23,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = var.pool_name
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-medium"
@@ -40,13 +40,13 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
 resource "google_compute_address" "load_balancer_ip" {
   name    = "load-balancer-ip"
-  region  = "europe-north1"
+  region  = var.region
   project = "${var.proj_name}"
 }
 
 resource "google_compute_address" "db_internal_ip" {
   name         = "db-internal-ip"
   address_type = "INTERNAL"
-  region  = "europe-north1"
+  region  = var.region
   project = "${var.proj_name}"
 }
