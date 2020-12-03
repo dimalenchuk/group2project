1fc96c7624fd9744288a3a7ef5a151ddb2ae698a
Dmytro Lenchuk <dimalenchuk@gmail.com>
2020-12-03 23:59:53 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,11 +1,19 @@
+data "terraform_remote_state" "infrastructure" {
+  backend = "local"
+
+  config = {
+    path = "../infrastructure/main.tf"
+  }
+}
+
 provider "kubernetes" {
   load_config_file = false
-  host     = google_container_cluster.primary.endpoint
-  token = data.google_client_config.default.access_token
+  host     = data.terraform_remote_state.infrastructure.google_container_cluster.primary.endpoint
+  token = data.terraform_remote_state.infrastructure.data.google_client_config.default.access_token
 
-  client_certificate     = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+  client_certificate     = "${base64decode(data.terraform_remote_state.infrastructure.google_container_cluster.primary.master_auth.0.client_certificate)}"
+  client_key             = "${base64decode(data.terraform_remote_state.infrastructure.google_container_cluster.primary.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(data.terraform_remote_state.infrastructure.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
 }
 
 resource "kubernetes_deployment" "mysql_deploy" {
