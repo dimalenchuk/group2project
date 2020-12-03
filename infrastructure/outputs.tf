f4424b94f9e9986b8dc3f2ac4e639512b06120fb
Dmytro Lenchuk <dimalenchuk@gmail.com>
2020-12-04 01:25:37 +0200

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -18,3 +18,8 @@ output "ca_certificate" {
   value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
   sensitive = true
 }
+
+output "load_balancer_ip" {
+  value = google_compute_address.load_balancer_ip.address
+  sensitive = true
+}
