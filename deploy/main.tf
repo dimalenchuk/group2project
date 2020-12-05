cfbb7c4d16d77227503b9c984dc91144fc466ad4
Dmytro Lenchuk <dimalenchuk@gmail.com>
2020-12-05 21:39:56 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -96,7 +96,6 @@ resource "kubernetes_service" "mysql_service" {
 }
 
 resource "kubernetes_deployment" "wordpress_deploy" {
-  depends_on = ["kubernetes_service.mysql_service", "kubernetes_deployment.mysql_deploy"]
   metadata {
     name = "wordpress-deploy"
 
