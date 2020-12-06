61a8ed49aaf9e6ef8467486f442498ddc8d028b0
Dmytro Lenchuk <dimalenchuk@gmail.com>
2020-12-06 20:23:14 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -58,7 +58,7 @@ resource "kubernetes_deployment" "mysql_deploy" {
       spec {
         container {
           name  = "mysql"
-          image = "dimalenchuk/novinanodb:2"
+          image = "dimalenchuk/novinanodb:plswork"
 
           port {
             container_port = 3306
@@ -131,7 +131,7 @@ resource "kubernetes_deployment" "wordpress_deploy" {
       spec {
         container {
           name  = "wordpress"
-          image = "dimalenchuk/novinano:1"
+          image = "dimalenchuk/novinano:plswork"
 
           port {
             container_port = 80
