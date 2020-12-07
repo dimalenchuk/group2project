28e66fa378e9c81d6509f9aa291d3420d7df55f3
Dmytro Lenchuk <dimalenchuk@gmail.com>
2020-12-07 15:06:00 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -131,7 +131,7 @@ resource "kubernetes_deployment" "wordpress_deploy" {
       spec {
         container {
           name  = "wordpress"
-          image = "dimalenchuk/novinano:ssl3"
+          image = "dimalenchuk/novinano:ssl4"
 
           port {
             container_port = 80
